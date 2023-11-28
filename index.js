37d45bacd713698e47af51cc652934041f8ed50f
Philip Burrows <48831325+philManWithBeard@users.noreply.github.com>
2023-11-28 13:47:15 +0000

    Plan Work

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 // Data kept in separate file
 import { finances } from "./finance-data.js";
+
+// Calculates the total number of arrays in the 2d array
+
+// Calculates the total profit by reducing the profit value in the 2d array
+
+// Create an array of the monthly changes
+
+// Calculate Total Monthly Change
+
+// Calculate Average Change
+
+// Calculate Minimum Change
+
+// Calculate Month of Minimum Change
+
+// Calculate Maximum Change
+
+// Calculate Month of Maximum Change
+
+// Output Results
